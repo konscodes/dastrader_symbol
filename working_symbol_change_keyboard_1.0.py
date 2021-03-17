059c65c96e114783d9bef3d7e373a9aac1a2ca39
Kons <katashi.rain@gmail.com>
2021-03-17 13:26:54 +0900

    Adding comments and changing structure

--- working_symbol_change_keyboard_1.0.py
+++ working_symbol_change_keyboard_1.0.py
@@ -4,31 +4,14 @@ import pyperclip
 import time
 import pygetwindow as gw
 
-print(gw.getAllTitles())
+# Locating DAS window
+#print(gw.getAllTitles())
 #print(gw.getWindowsWithTitle('DASTrader'))
 das = gw.getWindowsWithTitle('DASTrader')[0]
-
 print('\nWorking \n')
 
-#Reduce copipaste somehow
-#Hotkey to only be active with ISW windows
-
-def hotkey1():
-    main_function("F1")
-
-def hotkey2():
-    main_function("F2")
-    
-def hotkey3():
-    main_function("F3")
-
-def hotkey4():
-    main_function("F4")
-
-def hotkey5():
-    main_function("F5")
-
-def main_function(hotkey):
+# Main function
+def main_function(scriptkey):
     active_window = gw.getActiveWindow().title
     if "DASTrader" in active_window:
         print("DAS!")
@@ -39,13 +22,34 @@ def main_function(hotkey):
         time.sleep(0.01)
         print(pyperclip.paste())
         das.activate()
-        keyboard.send(hotkey)
+        keyboard.send(scriptkey)
         keyboard.send("ctrl+v+enter")
         print("--- %s seconds ---" % (time.time() - start_time))
-   
-keyboard.add_hotkey('F1', hotkey1)
-keyboard.add_hotkey('F2', hotkey2)
-keyboard.add_hotkey('F3', hotkey3)
-keyboard.add_hotkey('F4', hotkey4)
-keyboard.add_hotkey('F5', hotkey5)
+
+# Pass DAS script hotkey into main function
+def scriptkey1():
+    main_function("F1")
+
+def scriptkey2():
+    main_function("F2")
+    
+def scriptkey3():
+    main_function("F3")
+
+def scriptkey4():
+    main_function("F4")
+
+def scriptkey5():
+    main_function("F5")
+
+# Assign Python hotkeys to DAS script hotkeys
+keyboard.add_hotkey('F1', scriptkey1)
+keyboard.add_hotkey('F2', scriptkey2)
+keyboard.add_hotkey('F3', scriptkey3)
+keyboard.add_hotkey('F4', scriptkey4)
+keyboard.add_hotkey('F5', scriptkey5)
 keyboard.wait()
+
+# TODO 
+# Reduce copipaste somehow
+# Hotkey to only be active with ISW windows
