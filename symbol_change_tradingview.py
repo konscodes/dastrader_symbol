da9f3d13a7559f78a195d37e5b19e439a08c4f0d
Kons <katashi.rain@gmail.com>
2021-08-12 15:23:36 +0900

    First working version, adding delay for typing

--- symbol_change_tradingview.py
+++ symbol_change_tradingview.py
@@ -1,6 +1,7 @@
 from pynput.keyboard import Key, Controller, GlobalHotKeys
 import pyperclip
 import time
+import random
 import pygetwindow as gw
 
 # Locating DAS window
@@ -8,7 +9,7 @@ import pygetwindow as gw
 #print(gw.getWindowsWithTitle('DASTrader'))
 das = gw.getWindowsWithTitle('DASTrader')[0]
 tv = gw.getWindowsWithTitle('%')[0]
-print('\nWorking \n')
+print('\nWorking \nPress F12 to exit')
 
 keyboard = Controller()
 
@@ -30,13 +31,14 @@ def press_key(dashotkey):
 
 def main_function(dashotkey):
     active_window = gw.getActiveWindow().title
-    title_match_list = ["Intensity", "Ultra", "lounge", "Monitor", "Scan", "Chat", "CHAT"]
+    title_match_list = ['Intensity', 'Ultra', 'lounge', 'Monitor', 'Scan', 'Chat', 'CHAT']
     if any(title in active_window for title in title_match_list):
         start_time = time.time()
         time.sleep(0.1)
         copy()
         time.sleep(0.01)
-        print(pyperclip.paste())
+        symbol = pyperclip.paste()
+        print(symbol)
         das.activate()
         press_key(dashotkey)
         paste()
@@ -44,13 +46,14 @@ def main_function(dashotkey):
         time.sleep(0.3)
         tv.activate()
         time.sleep(0.1)
-        symbol = pyperclip.paste()
-        keyboard.type(symbol)
-        time.sleep(0.1)
+        for character in symbol:
+            keyboard.type(character)
+            #delay = random.uniform(0, 2)
+            time.sleep(0.05)
         press_key(Key.enter)
-        print("--- %s seconds ---" % (time.time() - start_time))
+        print('--- %s seconds ---' % (time.time() - start_time))
     else:
-        print("I3 not active!")
+        print('I3 not active!')
 
 # Pass DAS script hotkey into main function
 def transfer_function1():
@@ -68,12 +71,16 @@ def transfer_function4():
 def transfer_function5():
     main_function(DASHOTKEY5)
 
+def exit_function():
+    exit()
+
 # Assign Python hotkeys to DAS script hotkeys
-HOTKEY1 = "<F1>"
-HOTKEY2 = "<F2>"
-HOTKEY3 = "<F3>"
-HOTKEY4 = "<F4>"
-HOTKEY5 = "<F5>"
+HOTKEY1 = '<F1>'
+HOTKEY2 = '<F2>'
+HOTKEY3 = '<F3>'
+HOTKEY4 = '<F4>'
+HOTKEY5 = '<F5>'
+HOTKEY6 = '<F12>'
 
 DASHOTKEY1 = Key.f1
 DASHOTKEY2 = Key.f2
@@ -86,7 +93,8 @@ with GlobalHotKeys({
         HOTKEY2 : transfer_function2,
         HOTKEY3 : transfer_function3,
         HOTKEY4 : transfer_function4,
-        HOTKEY5 : transfer_function5}) as MappedResult:
+        HOTKEY5 : transfer_function5,
+        HOTKEY6 : exit_function}) as MappedResult:
     MappedResult.join()
 
 # TODO 
